72c07695ae717e12099761cc334ad5120175e7eb
junji.terada <junji.terada@gmail.com>
2014-09-24 13:33:14 +0900

    connect to TimePro

--- app/src/main/java/org/honeroku/timepro/app/plugin/receiver/AttendanceReceiver.java
+++ app/src/main/java/org/honeroku/timepro/app/plugin/receiver/AttendanceReceiver.java
@@ -7,15 +7,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.squareup.otto.Subscribe;
+
 import org.honeroku.timepro.app.plugin.bundle.BundleManager;
 import org.honeroku.timepro.app.plugin.bundle.BundleScrubber;
 import org.honeroku.timepro.domain.entity.Account;
 import org.honeroku.timepro.domain.repository.AccountRepository;
+import org.honeroku.timepro.domain.service.TimeProService;
+import org.honeroku.timepro.domain.event.ClockInEvent;
+import org.honeroku.timepro.domain.event.ClockOutEvent;
+import org.honeroku.timepro.util.EventBus;
 
 
 public final class AttendanceReceiver extends BroadcastReceiver {
 
     private static final String TAG = AttendanceReceiver.class.getName();
+
+    private Context context;
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -40,13 +48,34 @@ public final class AttendanceReceiver extends BroadcastReceiver {
             return;
         }
 
-        // TODO: TimeProServiceを同期/非同期両対応化、もしくは非同期化ラッパーをapp.mainに逃す
+        EventBus.getInstance().register(this);
+        this.context = context;
         boolean isClockIn = bundle.getBoolean(BundleManager.BUNDLE_EXTRA_CLOCK_IN_OUT);
         if (isClockIn) {
-            Toast.makeText(context, "出勤記録", Toast.LENGTH_LONG).show();
+            TimeProService.clockIn(account);
         } else {
-            Toast.makeText(context, "退勤記録", Toast.LENGTH_LONG).show();
+            TimeProService.clockOut(account);
         }
+    }
+
+    @Subscribe
+    public void onClockInEvent(ClockInEvent event) {
+        if (event.isSuccess()) {
+            Toast.makeText(context, "出勤記録をつけました", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(context, event.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        EventBus.getInstance().unregister(this);
+    }
+
+    @Subscribe
+    public void onClockOutEvent(ClockOutEvent event) {
+        if (event.isSuccess()) {
+            Toast.makeText(context, "退勤記録をつけました", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(context, event.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        EventBus.getInstance().unregister(this);
     }
 
 }
